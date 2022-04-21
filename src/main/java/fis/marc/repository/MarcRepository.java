70699c63b7c5d647ff116d42cbff4df483138aa8
Bum <sbl133@naver.com>
2022-04-21 15:56:53 +0900

    [feat] QueryDsl 의존성 추가

--- src/main/java/fis/marc/repository/MarcRepository.java
+++ src/main/java/fis/marc/repository/MarcRepository.java
@@ -1,6 +1,10 @@
 package fis.marc.repository;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import fis.marc.domain.Marc;
+import fis.marc.domain.QMarc;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -8,11 +12,14 @@ import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
+import static fis.marc.domain.QMarc.marc;
+
 @Repository
 @RequiredArgsConstructor
 public class MarcRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory jpaQueryFactory;
 
     public Optional<Marc> findOne(Long id) {
         return Optional.ofNullable(em.find(Marc.class, id));
