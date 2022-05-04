a4fa7e82427328be525d85e8fbb8039cdf25937e
cy <cy@cyui-MacBookAir.local>
2022-05-04 15:37:07 +0900

    cors registry 추가

--- src/main/java/fis/marc/configurator/WebConfig.java
+++ src/main/java/fis/marc/configurator/WebConfig.java
@@ -3,13 +3,25 @@ package fis.marc.configurator;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.persistence.EntityManager;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     @Bean
     JPAQueryFactory jpaQueryFactory(EntityManager em){
         return new JPAQueryFactory(em);
     }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*", "http://localhost/*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedMethods("*");
+    }
+
 }
